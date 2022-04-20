9b50f659193957b847addff4bcdadd85b60b9229
jsnyder159 <jw.snyder159@gmail.com>
2022-04-20 01:11:17 -0400

    added slides for portfolio

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -5,12 +5,12 @@ export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand">Jason Snyder</a>
+                <div className="navbar-brand">Jason Snyder</div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
+                    <ul className="navbar-nav ms-auto align-bottom">
                         <Link to='/'>
                         <li className="nav-item nav-link active">About Me</li>
                         </Link>
