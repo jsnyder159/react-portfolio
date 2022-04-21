edb2f379c9f815c78fa9e554cb225b0f9b63db17
jsnyder159 <jw.snyder159@gmail.com>
2022-04-21 01:45:23 -0400

    updated about me section and styles on it and my portfolio

--- portfolio/src/components/pages/about/About.js
+++ portfolio/src/components/pages/about/About.js
@@ -9,12 +9,13 @@ function About() {
                 <img className="gears col-4" src={gears} alt="beared mans head surround by gears"></img>
                 <h2 className="title col-1">About Me</h2>
                 <p className="para col-7">
-                    Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                    Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                    mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                    lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                    imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                    in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+                    My names Jason and I am working towards being a full stack web developer.  I spent the majority of the last decade working in the Import/Export
+                    industry at docks in NJ and PA as a Marine Superintendent.  Meeting people from all across the world and working with people from all kinds of 
+                    backgrounds was an enlightening experience.  I enjoy being in a constant state of learning and love problem solving.  My attention is drawn to 
+                    things that are dynamic and ever evolving.  I started teaching myself Python towards the end of 2021 and decided that I needed to make a change
+                    in careers where I made coding my focus.  My immediate goals are to work as hard as I can, focus on refining my skills while using the MERN stack,
+                    continue learning something new everyday, and find a job in web development.  Long term, I would like to combine the skills that I've gained from 
+                    my time working in Import/Export with the skills I am and continue to gain in coding to work my way up to being a Project Manager.
                 </p>
             </div>
         </div>
