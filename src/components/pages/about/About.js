cc2b40388177281928bd20a2870a8d11c9a880f4
jsnyder159 <jw.snyder159@gmail.com>
2022-05-23 19:38:56 -0400

    updated about me

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import gears from '../../../images/profile-gear.png'
-import './about.css'
+import React from "react";
+import gears from "../../../images/profile-gear.png";
+import "./about.css";
 
 function About() {
-    return (
-        <div className="about">
-            <div className="d-flex row">
-                <img className="gears col-4" src={gears} alt="beared mans head surround by gears"></img>
-                <h2 className="title col-1">About Me</h2>
-                <p className="para col-7">
-                    My names Jason and I am working towards being a full stack web developer.  I spent the majority of the last decade working in the Import/Export
-                    industry at docks in NJ and PA as a Marine Superintendent.  Meeting people from all across the world and working with people from all kinds of 
-                    backgrounds was an enlightening experience.  I enjoy being in a constant state of learning and love problem solving.  My attention is drawn to 
-                    things that are dynamic and ever evolving.  I started teaching myself Python towards the end of 2021 and decided that I needed to make a change
-                    in careers where I made coding my focus.  My immediate goals are to work as hard as I can, focus on refining my skills while using the MERN stack,
-                    continue learning something new everyday, and find a job in web development.  Long term, I would like to combine the skills that I've gained from 
-                    my time working in Import/Export with the skills I am and continue to gain in coding to work my way up to being a Project Manager.
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="about">
+      <div className="d-flex row">
+        <img
+          className="gears col-4"
+          src={gears}
+          alt="beared mans head surround by gears"
+        ></img>
+        <h2 className="title col-1">About Me</h2>
+        <p className="para col-7">
+          My names Jason and I am a full stack web developer. I spent the
+          majority of the last decade working in the Import/Export industry at
+          docks in NJ and PA as a Marine Superintendent. Meeting people from all
+          across the world and working with people from all kinds of backgrounds
+          was an enlightening experience. I started teaching myself Python
+          towards the end of 2021 and decided that I needed to make a change in
+          careers where I made coding my focus. I joined and completed the
+          Rutger's Mern Full Stack Web Dev Bootcamp in 2022. I had the pleasure
+          to meet and work with some really awesome people. I'm continuing my
+          development knowledge base with more classes with a focus on Mongodb,
+          Express, React, Node, and Python. With that experience I am going to
+          continue to work as hard as I can, refine my skills while focusing on
+          using the MERN stack, continue to learn something new everyday, and
+          find a job in web development.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
